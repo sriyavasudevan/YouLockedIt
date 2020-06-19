b2ec2d6ee1ad79bd853723057ffac00d4838f2b7
Sriya Madapusi Vasudevan <developments.sriya@gmail.com>
2020-06-18 21:02:37 -0400

    made legendary app, available in appstore, jk

--- YouLockedIt/ViewController.swift
+++ YouLockedIt/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showMessage(sender: UIButton) {
 
+        if let textOnButton = sender.titleLabel?.text {
+            
+            if textOnButton == "Door: Unlocked" {
+                 sender.setTitle("Door: Locked", for: .normal)
+                 sender.backgroundColor = UIColor.green
+                
+            } else if textOnButton == "Car: Unlocked" {
+                sender.setTitle("Car: Locked", for: .normal)
+                sender.backgroundColor = UIColor.green
+                
+            } else if textOnButton == "Door: Locked" {
+                sender.setTitle("Door: Unlocked", for: .normal)
+                sender.backgroundColor = UIColor.red
+                
+            } else if textOnButton == "Car: Locked" {
+                sender.setTitle("Car: Unlocked", for: .normal)
+                sender.backgroundColor = UIColor.red
+            }
+        }
+    }
 }
 
